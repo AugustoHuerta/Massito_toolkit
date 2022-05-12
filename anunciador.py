7ba9b52dd18e5709be207184a3b836a5187367b9
AugustoHuerta <augustohuertacopywriting@gmail.com>
2022-05-12 16:17:56 -0500

    2 tiendas más. Anunciar la instalación de los bots en un programa. Anunciar noticias semanales listo

--- anunciador.py
+++
@@ -1,2 +0,0 @@
-### Un programa para automatizar las buenas nuevas a los admins y coordinador de la zona.
-# Decirles sobre Madam Ventas, el dashboard de google y los mantenimientos que se realizaran a la app
